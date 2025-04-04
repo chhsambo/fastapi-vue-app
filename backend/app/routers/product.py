76eeb1b22c7b04ab81d8e4bef3165be30fc87980
Sambo Chhay <sambo@pipay.com>
2025-04-04 16:37:37 +0700

    Add pagination in products

--- backend/app/routers/product.py
+++ backend/app/routers/product.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import select, asc, desc
 from app.db import get_db
 from app.models.product import Product
 from app.schemas.product import ProductCreate, ProductUpdate
@@ -8,8 +9,17 @@ router = APIRouter()
 
 
 @router.get("/products")
-def index(db: Session = Depends(get_db)):
-    return db.query(Product).all()
+def index(page: int = 1, size: int = 10, order = "id", direction = "asc", db: Session = Depends(get_db)):
+    sort_direction = asc if direction == "asc" else desc 
+    products = (
+        db.query(Product)
+        .order_by(sort_direction(getattr(Product, order)))
+        .offset((page - 1) * size)
+        .limit(size)
+        .all()
+    )
+    return products
+    # return db.query(Product).all()
 
 
 @router.get("/products/{id}")
